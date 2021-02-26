fbc3a5804421b2881534d66b363be487f5e4c445
colossatr0n <29556317+colossatr0n@users.noreply.github.com>
2021-02-26 15:53:48 -0700

    - A receiver can now be specified on the CLI with --receiver so that an email can be sent to a different email than the sender's. - Listing descriptions can now be shortened by specifying the number of lines desired with --head. - A character limit can be specified with --char-limit. This limits how many characters can be sent in a single message. If additional listings exceed the character limit, they'll be sent in additional messages. This is useful when sending emails as SMS messages to phones. Note that the subject line is included in the character count as it is included in the message body when sent as an SMS. - send_email only sends email. Does not do any formatting of messages. - send_email now is now send_emails and can send multiple emails with one smtp login. - msn.com is now recognized by get_smtp. - Added timestamp to subject.

--- ksl_notify.py
+++ ksl_notify.py
@@ -12,16 +12,19 @@ from ksl import KSL, Listing
 
 
 # Message strings
-subject = "{query} search match on KSL Classifieds"
-sender = "KSL Notify <{mail}>"
-message_template = "\r\n".join([
+SUBJECT_TEMPLATE = "{query} search match on KSL Classifieds at {time} ({n} of {total})"
+SENDER_TEMPLATE = "KSL Notify <{mail}>"
+HEADER_TEMPLATE = "New match{plural} found for query {query}"
+BODY_TEMPLATE = "\r\n".join([
+    "{header}",
+    "",
+    "{listings_report}"])
+MESSAGE_TEMPLATE = "\r\n".join([
     "Subject: {subject}",
-    "To: {email}",
+    "To: {receiver}",
     "From: {sender}",
     "",
-    "New match{plural} found for query {query}"
-    "",
-    "{report}"])
+    "{body}"])
 
 
 def get_smtp(email):
@@ -31,7 +34,7 @@ def get_smtp(email):
         smtp_server = 'smtp.gmail.com:587'
     elif hostname == 'yahoo.com':
         smtp_server = 'smtp.mail.yahoo.com:587'
-    elif hostname == 'outlook.com' or hostname == 'hotmail.com':
+    elif hostname == 'outlook.com' or hostname == 'hotmail.com' or hostname == 'msn.com':
         smtp_server = 'smtp-mail.outlook.com:587'
     elif hostname == 'comcast.net':
         smtp_server = 'smtp.comcast.net:587'
@@ -59,61 +62,119 @@ def test_email_login(email, password, smtpserver):
     smtp.quit()
 
 
-def send_email(email, password, smtpserver, report, query, count):
-    smtp_addr, smtp_port = smtpserver.split(":")
-    smtp = smtplib.SMTP(smtp_addr, int(smtp_port))
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.login(email, password)
-    if count > 1:
-        plural = "es"
-    else:
-        plural = ""
-    body = message_template.format(subject=subject.format(query=query),
-                                   email=email,
-                                   sender=sender.format(mail=email),
-                                   plural=plural, query=query, report=report)
-    smtp.sendmail(sender.format(mail=email), email, body)
-    smtp.quit()
-    logging.debug("Sent this body:\n{body}".format(body=body))
-
-
-def gather_report(query_result, seen):
-    report = ""
+def format_listings(query_result, seen, head):
     new_seen = seen.copy()
-    for result in query_result:
-        if result.link not in seen:
-            report += ('*' * 25 +
-                       '\n{result.link}\n'
-                       '{result.title}\n'
-                       '${result.price} - {result.age} - '
-                       '{result.city}, {result.state}\n'
-                       '*  {result.description}\n\n'.format(**locals()))
+    listings_formatted = []
+    for listing in query_result:
+        if listing.link not in seen:
+            description = listing.description
+
+            # Take the first n lines of the description if head is specified
+            if head:
+                description = '\n'.join(listing.description.strip().split('\n')[:head])
+
+            listing_formatted = ('*' * 25 +
+                               '\n{listing.link}\n'
+                               '{listing.title}\n'
+                               '${listing.price} - {listing.age} - '
+                               '{listing.city}, {listing.state}\n'
+                               '*  {description}\n\n'.format(**locals()))
+
             # Kill non-ascii characters
-            report = report.encode('ascii', 'ignore').decode()
+            listing_formatted = listing_formatted.encode('ascii', 'ignore').decode()
+
+            listings_formatted.append(listing_formatted)
+
             # Track seen results
-            new_seen.append(result.link)
-    return report, new_seen
+            new_seen.append(listing.link)
+
+    return listings_formatted, new_seen
 
 
-def check_ksl(args, queries, seen, email, passwd, smtpserver):
+def check_ksl(args, queries, seen, receiver, sender, passwd, smtpserver):
     # create the thin object
     ksl = KSL()
+
+    head = args['head']
+    char_limit = args["char_limit"]
+
     for query, html_data in ksl.search(queries, **args):
         if query not in seen:
             seen[query] = []
 
         query_result = ksl.find_elements(html_data)
-        report, new_seen_list = gather_report(query_result, seen[query])
-        # If any new content, email results
-        if report:
-            count = len(new_seen_list) - len(seen[query])
-            logging.info("Sending email")
-            send_email(email, passwd, smtpserver, report, query, count)
+
+        listings, new_seen_list = format_listings(query_result, seen[query], head)
+
+        message_bodies = create_message_bodies(query, listings, char_limit)
+
+        # Email new results
+        messages = []
+        current_time = get_current_time()
+        for i, message_body in enumerate(message_bodies):
+            message = MESSAGE_TEMPLATE.format(subject=SUBJECT_TEMPLATE.format(query=query,
+                                                                              n=i + 1,
+                                                                              total=len(message_bodies),
+                                                                              time=current_time),
+                                              receiver=receiver,
+                                              sender=sender.format(mail=sender),
+                                              body=message_body)
+            messages.append(message)
+
+        send_emails(receiver, sender, passwd, smtpserver, messages)
+
+        if len(message_bodies) == 0:
+            logging.info("No new search results found. No email sent.")
 
         # Save of results for next time
         seen[query] = new_seen_list
     return seen
+
+
+def send_emails(receiver, sender, password, smtpserver, messages):
+    smtp_addr, smtp_port = smtpserver.split(":")
+    smtp = smtplib.SMTP(smtp_addr, int(smtp_port))
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login(sender, password)
+
+    for i, message in enumerate(messages):
+        logging.info("Sending email {n} of {total}".format(n=i + 1, total=len(messages)))
+        smtp.sendmail(sender.format(mail=sender), receiver, message)
+        logging.debug("Sent this message:\n{message}".format(message=message))
+
+    smtp.quit()
+
+
+def get_current_time():
+    return datetime.datetime.now().strftime("%H:%M")
+
+
+def create_message_bodies(search_term, listings, char_limit):
+    header = HEADER_TEMPLATE.format(plural="es" if len(listings) > 0 else "", query=search_term)
+    subject_count = len(SUBJECT_TEMPLATE.format(query=search_term,
+                                                n=len(listings),
+                                                total=len(listings),
+                                                time=get_current_time()))
+
+    message_bodies = []
+    listings_report = ""
+
+    # If there's a character limit, break the report into parts so that no part exceeds char_limit.
+    for listing in listings:
+        if char_limit:
+            # If listing pushes message body past character count, store message body without adding the listing.
+            # Note: Subject is included in char count since it's included in the message body when sent through SMS.
+            if len(listings_report) + len(listing) + len(header) + subject_count > char_limit:
+                message_bodies.append(BODY_TEMPLATE.format(header=header, listings_report=listings_report))
+                listings_report = ""
+
+        listings_report += listing
+
+    if len(listings) > 0:
+        message_bodies.append(BODY_TEMPLATE.format(header=header, listings_report=listings_report))
+
+    return message_bodies
 
 
 def main(args):
@@ -134,15 +195,19 @@ def main(args):
 
     # Get needed controls
     loop_delay = args.pop('time') * 60
-    email = args.pop('email', None)
+    sender = args.pop('email', None)
     smtpserver = args.pop('smtpserver', None)
-    if not email:
-        email = input("Enter email address to use: ")
+    if not sender:
+        sender = input("Enter email address to use: ")
     if not smtpserver:
-        smtpserver = get_smtp(email)
+        smtpserver = get_smtp(sender)
     passwd = getpass.getpass("Enter password for sending email from {email}: "
-                             .format(email=email))
-    test_email_login(email, passwd, smtpserver)
+                             .format(email=sender))
+    test_email_login(sender, passwd, smtpserver)
+
+    receiver = args.pop('receiver', None)
+    if not receiver:
+        receiver = sender
 
     # Fork to background
     foreground = args.pop('foreground')
@@ -163,7 +228,7 @@ def main(args):
     today = None
     while True:
         try:
-            seen = check_ksl(args, queries, seen, email, passwd, smtpserver)
+            seen = check_ksl(args, queries, seen, receiver, sender, passwd, smtpserver)
             # log seen list daily for debug
             if (today != datetime.date.today()):
                 logging.debug("seen list: %s"%(seen))
@@ -184,12 +249,19 @@ def main(args):
             try:
                 exc_txt = str(e)
                 if exception_count > exception_thresh:
-                    send_email(email, passwd, smtpserver, str(queries),
-                               "Exception in script detected.\n"
-                               "Exception count %d\n"
-                               "The script will die after the count reaches 10\n"
-                               "%s"
-                               % (exception_count / 10, exc_txt), 0)
+                    logging.info("Sending exception message to {sender}".format(sender=sender))
+                    send_emails(sender,
+                                sender,
+                                passwd,
+                                smtpserver,
+                                [MESSAGE_TEMPLATE.format(subject="KSL Notifier Failure",
+                                                       receiver=sender,
+                                                       sender=sender,
+                                                       body="Exception in script detected.\n"
+                                                            "Exception count %d\n"
+                                                            "The script will die after the count reaches 10\n"
+                                                            "%s"
+                                                            % (exception_count / 10, exc_txt))])
             except:
                 pass
             # If there is something more basic failing, the count trigger
@@ -207,10 +279,13 @@ if __name__ == '__main__':
     )
 
     p.add_argument('--email', default=None,
-                   help='email address from which to both send and receive')
+                   help='email address from which to send. '
+                        'If --receiver is not specified, this email will also be used as the receiver.')
     p.add_argument('--smtpserver', default='',
                    help='email SMTP server:port, should be unneeded for '
-                   'gmail, outlook, hotmail, yahoo, or comcast')
+                   'gmail, outlook, hotmail, msn, yahoo, or comcast')
+    p.add_argument('--receiver', default=None,
+                   help='email address to send the email to. Defaults to --email value.')
     p.add_argument('-t', '--time', nargs='?', default=10, const=int, type=int,
                    help='Number of minutes to wait between searches')
     p.add_argument('-l', '--logfile', default=None,
@@ -241,6 +316,11 @@ if __name__ == '__main__':
     p.add_argument('-n', '--perPage', default=None,
                    help='Number of results to include in search results.'
                    '(Does not seem to work!)')
+    p.add_argument('--head', default=None, type=int,
+                   help="Number of lines to include from the listing's description.")
+    p.add_argument('--char-limit', default=None, type=int,
+                   help="Number of characters allowed in the message body. " 
+                        "Listings that exceed the character count will be sent in additional messages.")
     p.add_argument('-r', '--reverse', action='store_const',
                    default=0, const=1, dest='sort',
                    help='If included, query will sort oldest to newest. '
